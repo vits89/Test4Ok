23bf5aee3d3eb5b56e4773d05419f69fff5135a1
Vitalij Sorokin <sv89@tut.by>
2020-01-04 23:18:34 +0300

    Изменён файл script.js

--- WebApplication1/wwwroot/js/script.js
+++ WebApplication1/wwwroot/js/script.js
@@ -1,12 +1,17 @@
-"use strict";
+const viewModel = (() => {
+    const apiUrl = "/api/news";
 
-const apiUrl = "http://localhost:5728/api/newsapi";
+    const $news = $("#news"),
+        $requestForm = $("#request-form");
 
-$(() => {
-    $("form#request-form").on("submit", function(event) {
+    const init = () => {
+        $requestForm.on("submit", handleRequestFormSubmit).submit();
+    };
+
+    const handleRequestFormSubmit = function(event) {
         event.preventDefault();
 
-        let formData = {
+        const formData = {
             newsSource: $(this).find("select[name=NewsSource]").val(),
             orderByDate: $(this).find("input[type=radio][name=OrderByDate]:checked").val()
         };
@@ -14,97 +19,99 @@ $(() => {
         $.ajax({
             data: formData,
             url: apiUrl
-        }).done(showResponse);
-    }).submit();
-});
+        }).done(showNews);
+    };
+    const handlePaginatorClick = function (event) {
+        event.preventDefault();
 
-function showResponse(data) {
-    let container = $("div#news");
+        const requestData = {
+            newsSource: $(this).data("news-source"),
+            orderByDate: $(this).data("order-by-date"),
+            page: $(this).text()
+        };
 
-    $(container).empty();
-
-    if (data.news.length > 0) {
-        $(container).append(createTable(data.news));
-
-        if (data.pagingInfo.totalPages > 0) {
-            $(container).append("<br>");
-            $(container).append(createPaginator(data.pagingInfo, data.requestFormData));
-        }
-    } else {
-        $(container).append("<p>Нет новостей</p>");
-    }
-}
-function createTable(news) {
-    let body = $("<tbody></tbody>"),
-        head = $("<thead></thead>"),
-        row = $("<tr></tr>");
-
-    [
-        "Источник",
-        "Название новости",
-        "Описание новости",
-        "Дата публикации"
-    ].forEach(value => {
-        let element = $("<th></th>").text(value);
-
-        $(row).append(element);
-    });
-
-    $(head).append(row);
-
-    news.forEach(value => {
-        const htmlString = "<td></td>";
-
-        let row = $("<tr></tr>"),
-            publishDate = new Date(value.publishDate);
-
-        $(row).append([
-            $(htmlString).text(value.newsSource.name),
-            $(htmlString).text(value.title),
-            $(htmlString).html($.parseHTML(value.description)),
-            $(htmlString).text(publishDate.toLocaleString())
-        ]);
-
-        $(body).append(row);
-    });
-
-    return $("<table></table>").append([head, body]);
-}
-function createPaginator(pagingInfo, requestData) {
-    let anchorElement,
-        anchorDataValues = { },
-        paginator = $("<div></div>").on("click", "a", paginatorClickEventHandler);
-
-    for (let p = 1; p <= pagingInfo.totalPages; p++) {
-        if (p === pagingInfo.currentPage) {
-            $(paginator).append(p);
-        } else {
-            anchorDataValues.newsSource = requestData.newsSource;
-            anchorDataValues.orderByDate = requestData.orderByDate;
-
-            anchorElement = $("<a></a>", { href: "#" }).data(anchorDataValues).text(p);
-
-            $(paginator).append(anchorElement);
-        }
-        if (p < pagingInfo.totalPages) {
-            $(paginator).append(" ");
-        }
-    }
-
-    return paginator;
-}
-
-function paginatorClickEventHandler(event) {
-    event.preventDefault();
-
-    let request = {
-        newsSource: $(this).data("news-source"),
-        orderByDate: $(this).data("order-by-date"),
-        page: $(this).text()
+        $.ajax({
+            data: requestData,
+            url: apiUrl
+        }).done(showNews);
     };
 
-    $.ajax({
-        data: request,
-        url: apiUrl
-    }).done(showResponse);
-}
+    const showNews = data => {
+        $news.empty();
+
+        if (data.news.length > 0) {
+            $news.append(createTable(data.news));
+
+            if (data.pagingInfo.totalPages > 0) {
+                $news.append("<br>")
+                    .append(createPaginator(data.pagingInfo, data.requestFormData));
+            }
+        } else {
+            $news.append("<p>Нет новостей</p>");
+        }
+    };
+    const createTable = news => {
+        const $body = $("<tbody></tbody>"),
+            $head = $("<thead></thead>"),
+            $row = $("<tr></tr>");
+
+        [
+            "Источник",
+            "Название новости",
+            "Описание новости",
+            "Дата публикации"
+        ].forEach(value => {
+            const $element = $("<th></th>").text(value);
+
+            $row.append($element);
+        });
+
+        $head.append($row);
+
+        news.forEach(value => {
+            const htmlString = "<td></td>",
+                publishDate = new Date(value.publishDate);
+
+            const $row = $("<tr></tr>");
+
+            $row.append([
+                $(htmlString).text(value.newsSource.name),
+                $(htmlString).text(value.title),
+                $(htmlString).html($.parseHTML(value.description)),
+                $(htmlString).text(publishDate.toLocaleString())
+            ]);
+
+            $body.append($row);
+        });
+
+        return $("<table></table>").append([$head, $body]);
+    };
+    const createPaginator = (pagingInfo, requestData) => {
+        const $paginator = $("<div></div>").on("click", "a", handlePaginatorClick);
+
+        for (let p = 1; p <= pagingInfo.totalPages; p++) {
+            if (p === pagingInfo.currentPage) {
+                $paginator.append(p);
+            } else {
+                const anchorDataValues = {
+                    newsSource: requestData.newsSource,
+                    orderByDate: requestData.orderByDate
+                };
+
+                const $anchorElement = $("<a></a>", { href: "#" }).data(anchorDataValues).text(p);
+
+                $paginator.append($anchorElement);
+            }
+
+            if (p < pagingInfo.totalPages) {
+                $paginator.append(" ");
+            }
+        }
+
+        return $paginator;
+    };
+
+    return { init };
+})();
+
+$(viewModel.init);
